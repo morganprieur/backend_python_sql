88ab8d656e6158e69ea331d31a75aed24f3658b9
morgan_p <morgan.prieur@tetradis.com>
2024-03-14 12:20:41 +0100

    Installation project + models ok.

--- README.md
+++ README.md
@@ -8,22 +8,53 @@ Développer une application (CLI) de gestion de clientèle (CRM), avec Python et
 ## Environnemnet virtuel Pipenv 
 [Doc de Pipenv](https://post-it.pycolore.fr/post-it/python/pipenv) 
 
-*  Créer un projet : `pipenv --python 3.11` -> créer Pipfile 
+*  Créer un projet : `pipenv --python 3.11` -> crée le Pipfile 
 *  Installer les dépendances du projet : `pipenv install` (ajouter des dépndances, comme "dev" par exemple : `--dev`) 
 *  Activer l'environnement virtuel : `pipenv shell` 
     --> Le message "Launching subshell in virtual environment..." est afifché dans la console  
     --> le nom du termianl devient "pipenv" 
 *  Lancer des commandes à l'intérieur de l'env virtuel : `pipenv run <command>` 
-*  Lancer le programme : `pipenv ruen python chemin/du/projet.py`    
+*  Lancer le programme : `pipenv run python chemin/du/projet.py`    
+
+
+## Container Docker pour la BDD 
+* Créer un fichier `.env` pour stocker les données sensibles : 
+    - POSTGRES_DB = <le nom de la BDD> 
+    - POSTGRES_USER = <le nom de l'utilisateur admin> 
+    - POSTGRES_PASSWORD = <le mot de passe de l'admin> 
+    - DB_PORT = <le port où servir la BDD> 
+    - ADMINER_PORT = <le port où servir Adminer> 
+    Docker utilise ces données pour créer la BDD dans le container. 
+* Configurer le fichier compose.yaml en fonction de vos préférences (nom de container, par exemple). 
+
 
 ## Installation 
 
 1. Télécharger le dossier .zip 
 2. Extraire les fichiers dans un dossier local dédié au projet 
-3. installer et lancer l'environnement virtuel 
-4. Créer la bdd ? *** 
+3. installer et lancer l'environnement virtuel (voir ci-dessus) 
+4. Créer et lancer le container Docker : 
+    taper dans un terminal `docker compose up --build` 
+    * Après chaque modification du fichier compose.yaml : penser à : 
+    - supprimer les volumes dans l'outil de gestion de Docker 
+    - "re-builder" le container (`docker compose up--build`) 
+    * Pour relancer le container sans avoir modifié le compose.yaml : `docker compose up` 
+    * Fermer le container : `ctrl+c`, ou depuis un autre terminal : `compose down` 
+5. Vérifier qu'Adminer peut se connecter à la BDD : 
+    - visiter l'adresse http://localhost:${ADMINER_PORT} 
+    - sélectionner "Postgresql" dans le menu déroulant 
+    + taper les informations suivantes dans le formulaire : 
+        - host : <le nom du service> 
+        - utilisateur : POSTGRES_USER 
+        - mot de passe : POSTGRES_PASSWORD 
+        - nom de base de données : POSTGRES_DB 
+    - envoyer le formulaire 
+    --> Adminer affiche la bdd. 
+
+**Compléter** 
+<!-- 6. Lancer l'application ?  -->
+<!-- 6. Données ?  -->
 
 
 
-**Compléter à partir d'ici** 
 
---
+++ epic_events/models.py
@@ -0,0 +1,177 @@
+
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean 
+from sqlalchemy.orm import relationship, declarative_base 
+
+
+Base = declarative_base() 
+
+class User(Base): 
+    __tablename__ = 'users' 
+
+    id = Column( 
+        Integer, 
+        primary_key=True, 
+        index=True 
+    ) 
+    name = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    email = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    #TODO: password_hash + max_length 
+    password = Column( 
+        String, 
+        unique=True, 
+    ) 
+    phone = Column( 
+        String, 
+        unique=True 
+    ) 
+    department = Column( 
+        String, 
+        unique=False, 
+    ) 
+
+    client = relationship("Client", back_populates="users") 
+    event = relationship("User", back_populates="users") 
+
+    def __repr__(self): 
+        return f'User {self.name}' 
+
+class Client(Base): 
+    __tablename__ = 'clients' 
+
+    id = Column( 
+        Integer, 
+        unique=True, 
+        index=True 
+    ) 
+    name = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    email = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    phone = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    corporation_name = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    #TODO Auto *** 
+    created_at = Column( 
+        DateTime 
+    ) 
+    #TODO Auto *** 
+    updated_at = Column( 
+        DateTime 
+    ) 
+    sales_contact_id = Column( 
+        Integer, 
+        ForeignKey("user.id"), 
+    ) 
+
+    sales_contact = relationship('User', back_populates="clients") 
+    contract = relationship("Contract", back_populates="clients") 
+
+    def __repr__(self): 
+        return f'Client {self.name}' 
+
+
+class Contract(Base): 
+    __tablename__ = 'contracts' 
+    id = Column( 
+        Integer, 
+        unique=True, 
+        index=True 
+    ) 
+    client_id = Column( 
+        ForeignKey('clients.id') 
+    ) 
+    # A multiplier par 100 pour retrouver un prix avec centimes 
+    amount = Column( 
+        Integer 
+    ) 
+    # A multiplier par 100 pour retrouver un prix avec centimes 
+    paid_amount = Column( 
+        Integer 
+    ) 
+    is_signed = Column( 
+        Boolean 
+    ) 
+    #TODO Auto *** 
+    created_at = Column( 
+        DateTime 
+    ) 
+
+    client = relationship("User", back_populates="contracts") 
+    event = relationship("Event", back_populates="contracts") 
+
+    def __repr__(self): 
+        return f'Contract {self.id}, client {self.client_id}' 
+
+
+class Event(Base): 
+    __tablename__ = 'events' 
+
+    id = Column( 
+        Integer, 
+        unique=True, 
+        index=True 
+    ) 
+    name = Column( 
+        String, 
+        unique=True, 
+        index=True 
+    ) 
+    contract_id = Column( 
+        Integer, 
+        ForeignKey('contracts.id') 
+    ) 
+    # client_contact = Column( 
+    #     string 
+    # ) 
+    start_datetime = Column( 
+        DateTime 
+    ) 
+    end_datetime = Column( 
+        DateTime 
+    ) 
+    support_contact_id = Column( 
+        Integer, 
+        ForeignKey('users.id') 
+    ) 
+    location = Column( 
+        String 
+    ) 
+    Attendees = Column( 
+        Integer 
+    ) 
+    notes = Column( 
+        String 
+    ) 
+
+    support_contact = relationship("User", back_populates="events") 
+    contract = relationship("Contract", back_populates="events") 
+
+    def __repr__(self): 
+        return f'Event {self.name}' 
+
+
+
+
+
+

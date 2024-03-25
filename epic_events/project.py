6e7418f124d087763e0d1911d9dd90460ede677d
morgan_p <morgan.prieur@tetradis.com>
2024-03-25 09:48:31 +0100

    Added Sentry watch.

--- epic_events/project.py
+++ epic_events/project.py
@@ -1,4 +1,17 @@
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://92f2fd7678cf64d3efff193b8390cc2e@o4506970696646656.ingest.us.sentry.io/4506970703200256",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+) 
+
 # import this 
 
 from controller import Controller 
@@ -17,6 +30,7 @@ session = PromptSession()
 def main(mode='pub'): 
 
     print(f'hello main {datetime.now()}') 
+    division_by_zero = 1 / 1 
     controller = Controller() 
     controller.start(mode) 
     # view = Views() 

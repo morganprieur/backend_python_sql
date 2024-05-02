136da2502a6b9c296a7da44e195d9af45ff5f9b3
morgan_p <morgan.prieur@tetradis.com>
2024-05-02 11:04:39 +0200

    Tests w/out the 'refresh' + facto user connexion into tests ok. TODO: check Controller.

--- epic_events/tests/helpers_te_ts.py
+++ epic_events/tests/helpers_te_ts.py
@@ -21,7 +21,6 @@ class ConnectTest():
     def connect_user(cls): 
         """ Test the user's token. 
         """ 
-        # registered = cls.manager.decrypt_token() 
         role_dict = { 
             'admin': { 
                 'email': 'admin@mail.org', 
@@ -42,7 +41,7 @@ class ConnectTest():
                 'dept': 'SUPPORT' 
             } 
         } 
-        connectUser = cls.manager.select_one_user( 
+        cls.connectUser = cls.manager.select_one_user( 
         	'email', 
         	role_dict[cls.role]['email'] 
         ) 
@@ -51,7 +50,7 @@ class ConnectTest():
         if row is not None: 
         	cls.permission = cls.manager.verify_token( 
         		role_dict[cls.role]['email'], 
-        		connectUser.department.name, 
+        		cls.connectUser.department.name, 
         		row 
         	) 
         	if cls.permission == 'past': 
@@ -62,7 +61,7 @@ class ConnectTest():
         	    if pw_check: 
         	        new_token = cls.manager.get_token(120, { 
         	            'email': role_dict[cls.role]['email'], 
-        	            'dept': connectUser.department.name 
+        	            'dept': cls.connectUser.department.name 
         	        }) 
         	        cls.manager.register_token( 
         	            role_dict[cls.role]['email'], 
@@ -74,14 +73,12 @@ class ConnectTest():
         	        ) 
         	        cls.permission = cls.manager.verify_token( 
         	            role_dict[cls.role]['email'], 
-        	            connectUser.department.name, 
+        	            cls.connectUser.department.name, 
         	            row 
         	        ) 
         	        assert cls.permission == role_dict[cls.role]['dept'] 
-        	        # assert 1 == 2 
         	    else: 
         	        assert not cls.permission 
-        	        assert 2 == 3 
         else: 
             pw_check = cls.manager.check_pw( 
                 role_dict[cls.role]['email'], 
@@ -90,7 +87,7 @@ class ConnectTest():
             if pw_check: 
                 new_token = cls.manager.get_token(120, { 
                     'email': role_dict[cls.role]['email'], 
-                    'dept': connectUser.department.name 
+                    'dept': cls.connectUser.department.name 
                 }) 
                 cls.manager.register_token( 
                     role_dict[cls.role]['email'], 
@@ -102,13 +99,11 @@ class ConnectTest():
                 ) 
                 cls.permission = cls.manager.verify_token( 
                     role_dict[cls.role]['email'], 
-                    connectUser.department.name, 
+                    cls.connectUser.department.name, 
                     row 
                 ) 
                 assert cls.permission == role_dict[cls.role]['dept'] 
-                assert 3 == 4 
             else: 
                 assert not cls.permission 
-                assert 4 == 5 
 
 			# if cls.permission in ['GESTION', 'COMMERCE', 'SUPPORT']: 
--- epic_events/tests/test_11_admin_create_support_dept.py
+++ epic_events/tests/test_11_admin_create_support_dept.py
@@ -1,6 +1,6 @@
 
-# from epic_events.helpers import decorator_verify_jwt 
 from epic_events.manager import Manager 
+from helpers_te_ts import ConnectTest 
 
 import unittest 
 import json 
@@ -8,75 +8,45 @@ import os
 from datetime import datetime 
 
 
-
-class Superuser3Test(unittest.TestCase): 
-    """ Config test files. 
-    """ 
-    @classmethod
-    def setUp(cls): 
-        # # view = Views() 
-        cls.manager = Manager() 
-        cls.manager.connect() 
-        cls.manager.create_session() 
-
-    @classmethod
-    def test_1_verify_admin_token(cls): 
-    	""" Test the admin user's token. 
-    		Expect his permission dept == 'GESTION'. 
-    	""" 
-    	# registered = cls.manager.decrypt_token() 
-    	connectEmail = 'admin@mail.org' 
-    	cls.connectUser = cls.manager.select_one_user('email', 'admin@mail.org') 
-    	if cls.manager.verify_if_token_exists(connectEmail): 
-    		cls.permission = cls.manager.verify_token( 
-                connectEmail, 
-                cls.connectUser.department.name 
-            ) 
-    		if cls.permission in ['GESTION', 'COMMERCE', 'SUPPORT']: 
-    		    assert cls.permission == 'GESTION' 
-    		elif cls.permission == 'past': 
-    		    pass_counter = 1 				
-    		    # file deepcode ignore NoHardcodedPasswords/test: Local project 
-    		    userEmail = 'admin@mail.org' 
-    		    userPass = 'pass_superuser1' 
-    		    if cls.manager.check_pw(userEmail, userPass): 
-    		        user_db = cls.manager.select_one_user('email', userEmail) 
-    		        assert user_db.department.name == 'gestion' 
-    		        token = cls.manager.get_token(5, { 
-    		            'email': userEmail, 
-    		            'dept': user_db.department.name 
-    		        }) 
-    		        cls.manager.register_token(userEmail, 'token', token) 
-    		        cls.permission = user_db.department.name.upper() 
-    		        assert cls.permission == 'GESTION' 
+class SuperuserTest(unittest.TestCase): 
+	""" Config test files. 
+	""" 
+	@classmethod 
+	def setUp(cls): 
+		cls.manager = Manager() 
+		cls.manager.connect() 
+		cls.manager.create_session() 
+		cls.helpers = ConnectTest('admin') 
+		cls.helpers.connect_user() 
 
 
-    @classmethod
-    def test_2_not_already_support_dept(cls): 
-        """ Test getting one dept with name 'support', if permission is 'GESTION'. 
-            Expect 2 deptartments registered into the DB 
-            and the none of them's name is 'support'. 
-    	""" 
-        if cls.permission == 'GESTION': 
-            depts_db = cls.manager.select_all_entities('depts') 
-            assert len(depts_db) == 2 
-            depts_names_list = [] 
-            for dept in depts_db: 
-                depts_names_list.append(dept.name) 
-            assert 'support' not in depts_names_list 
+	@classmethod
+	def test_2_not_already_support_dept(cls): 
+	    """ Test getting one dept with name 'support', if permission is 'GESTION'. 
+	        Expect 2 deptartments registered into the DB 
+	        and the none of them's name is 'support'. 
+		""" 
+	    if cls.helpers.permission == 'GESTION': 
+	        depts_db = cls.manager.select_all_entities('depts') 
+	        assert len(depts_db) == 2 
+	        depts_names_list = [] 
+	        for dept in depts_db: 
+	            depts_names_list.append(dept.name) 
+	        assert 'support' not in depts_names_list 
 
-    @classmethod
-    def test_3_creation_support_dept(cls): 
-        """ Test adding one dept, if permission is 'GESTION'. 
-            Expect 3 deptartments registered into the DB 
-            and the last's name is 'support'. 
-    	""" 
-        if cls.permission == 'GESTION': 
-            suppDept = cls.manager.add_entity('dept', { 
-                'name': 'support' 
-            }) 
-            items_db = cls.manager.select_all_entities('depts') 
-            assert len(items_db) == 3 
-            last_dept = items_db.pop() 
-            assert last_dept.name == 'support' 
+	@classmethod
+	def test_3_creation_support_dept(cls): 
+	    """ Test adding one dept, if permission is 'GESTION'. 
+	        Expect 3 deptartments registered into the DB 
+	        and the last's name is 'support'. 
+		""" 
+	    if cls.helpers.permission == 'GESTION': 
+	        suppDept = cls.manager.add_entity('dept', { 
+	            'name': 'support' 
+	        }) 
+	        items_db = cls.manager.select_all_entities('depts') 
+	        assert len(items_db) == 3 
+	        last_dept = items_db.pop() 
+	        assert last_dept.name == 'support' 
+	        # assert 1 == 2 
 
